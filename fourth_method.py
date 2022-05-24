6ecdd8747c9e3cc88c6cadb62f0b3069c618fb8e
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-24 00:25:16 -0300

    tirando a funcao len()

--- fourth_method.py
+++ fourth_method.py
@@ -2,14 +2,18 @@ import random
 list = [random.randrange(0, 1000, 1) for i in range(100)]
 print(list)
 
-for i in range(len(list)):
+lenght = 0
+for element in list:
+    lenght += 1
+
+for i in range(lenght):
     while i != 0 and list[i] > list[i-1]:
         aux = list[i]
         list[i] = list[i-1]
         list[i-1] = aux
         i = i - 1
 
-for i in range(len(list)):
+for i in range(lenght):
     if list[i] > list[i+1]:
         print(list[i+1])
         break
